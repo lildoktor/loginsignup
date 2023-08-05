4dcd001d84d7514ed1ebe0c2188c2b517e38ae77
Abdullah Kul <abdullahkul1998@gmail.com>
2023-08-05 16:14:11 +0300

    almost clean

--- app/src/main/java/com/example/codeseasy/com/firebaseauth/MainActivity.java
+++ app/src/main/java/com/example/codeseasy/com/firebaseauth/MainActivity.java
@@ -34,11 +34,10 @@ import java.time.format.DateTimeFormatter;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-    Button button, button_image, button_all;
+    Button button, btn_upload;
     TextView textView;
     FirebaseUser user;
-    private static final int PICK_IMAGE_REQUEST = 1;
-    private static final int YOUR_REQUEST_CODE = 2;
+    private static final int MM_CODE = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,8 +48,7 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.user_details);
         user = auth.getCurrentUser();
 
-        button_all = findViewById(R.id.all);
-        button_image = findViewById(R.id.upload);
+        btn_upload = findViewById(R.id.upload);
 
         if (user == null) {
             Intent intent = new Intent(getApplicationContext(), Login.class);
@@ -67,60 +65,19 @@ public class MainActivity extends AppCompatActivity {
             finish();
         });
 
-        button_image.setOnClickListener(view -> {
-            Intent intent = new Intent();
-            intent.setType("image/*");
-            intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-        });
-
-        button_all.setOnClickListener(view -> {
+        btn_upload.setOnClickListener(view -> {
             Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
             intent.setType("*/*");
             intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[] {"image/*", "video/*", "audio/*"});
-            startActivityForResult(intent, YOUR_REQUEST_CODE);
+            startActivityForResult(intent, MM_CODE);
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            try {
-                Uri uri = data.getData();
-                String timestamp = String.valueOf(Instant.now().getEpochSecond());
-                String uuid = user.getUid();
 
-                String filePath = uuid + "/" + timestamp + "/" + timestamp;
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                byte[] bytes = baos.toByteArray();
-                String mimeType = getContentResolver().getType(uri);
-
-                // Create metadata
-                StorageMetadata metadata = new StorageMetadata.Builder()
-                        .setContentType(mimeType)
-                        .build();
-                FirebaseStorage storage = FirebaseStorage.getInstance();
-                StorageReference storageRef = storage.getReference().child(filePath);
-                UploadTask uploadTask = storageRef.putBytes(bytes, metadata);
-                uploadTask.addOnSuccessListener(taskSnapshot -> {
-                    Toast.makeText(MainActivity.this, "Upload successful", Toast.LENGTH_LONG).show();
-                    // Handle successful upload here
-                }).addOnFailureListener(e -> {
-                    Log.e("Error1", e.getMessage());
-                    Toast.makeText(MainActivity.this, "Upload Failed -> " + e, Toast.LENGTH_LONG).show();
-                    e.printStackTrace();
-
-                    // Handle failed upload here
-                });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        else if(requestCode == YOUR_REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+        if(requestCode == MM_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
             try {
                 Uri uri = data.getData();
                 String timestamp = String.valueOf(Instant.now().getEpochSecond());
